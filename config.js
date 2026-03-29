7a537793564979986f71a57b7f530dfb99768c6c
sanderweg <140747509+sanderweg@users.noreply.github.com>
2026-03-29 19:52:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
-// Vul hier jouw Firebase web config in.
-// Deze gegevens krijg je in Firebase Console > Project settings > Your apps > Web app.
-// Zet hieronder ook de databaseURL van je NIEUWE Realtime Database instance.
-
 export const firebaseConfig = {
-  apiKey: "VUL_HIER_JE_API_KEY_IN",
-  authDomain: "VUL_HIER_JE_AUTH_DOMAIN_IN",
-  databaseURL: "https://VUL-HIER-JE-NIEUWE-DATABASE-URL-IN",
-  projectId: "VUL_HIER_JE_PROJECT_ID_IN",
-  storageBucket: "VUL_HIER_JE_STORAGE_BUCKET_IN",
-  messagingSenderId: "VUL_HIER_JE_MESSAGING_SENDER_ID_IN",
-  appId: "VUL_HIER_JE_APP_ID_IN"
+  apiKey: "AIzaSyAHJ_U7_H7rO1CLDEmgYm2bY-956R2B3jI",
+  authDomain: "karting-competitie.firebaseapp.com",
+  databaseURL: "https://karting-competitie-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "karting-competitie",
+  storageBucket: "karting-competitie.firebasestorage.app",
+  messagingSenderId: "915335846004",
+  appId: "1:915335846004:web:5fbc6592f60a93a9031921"
 };
